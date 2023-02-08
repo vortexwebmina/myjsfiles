ed1fb3123bbef1cd7cc7cebe26f8d5547c8dfd0a
Monalisa Octocat <octocat@gi.com>
2023-02-08 12:29:35 +0200

    a  ce

--- 163.js
+++ 163.js
@@ -1,7 +1,4 @@
- CURLOPT_URL => 'https://api.github.com/repos/vortexwebmina/myjsfiles/contents/163.js',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
+CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VE
